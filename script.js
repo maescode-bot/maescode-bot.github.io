52a3342fcd5800584271d4abba247cad95173a04
Abel Olayinka <56263750+maescode-bot@users.noreply.github.com>
2020-05-12 06:57:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(
+	function () {
+
+		$("#education").hide();
+		$("#work").hide();
+		$("#interest").hide();
+		$("#contact").hide();
+
+
+		$("#education_tab").click(function(){
+			$("#work").hide();
+			$("#interest").hide();
+			$("#contact").hide();
+			$("#whatido").hide();
+			$("#education").css('font-size','1.3em');
+			$("#education").show(2000);
+
+			
+			
+		})
+
+		$("#work_tab").click(function(){
+			$("#interest").hide();
+			$("#contact").hide();
+			$("#whatido").hide();
+			$("#education").hide();
+			$("#work").css('font-size','1.3em');
+			$("#work").show(2000);
+			
+		})
+
+		$("#interet_tab").click(function(){
+			$("#whatido").hide();
+			$("#education").hide();
+			$("#work").hide()
+			$("#contact").hide();
+			$("#interest").css('font-size','1.3em');
+			$("#interest").show(2000);
+		})
+
+		$("#contact_tab").click(function(){
+			$("#whatido").hide();
+			$("#education").hide();
+			$("#work").hide()
+			$("#interest").hide();
+			$("#contact").css('font-size','1.3em');
+			$("#contact").show(2000);
+		})
+
+
+	}
+);
